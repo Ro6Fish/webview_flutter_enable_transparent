7a425000caa2dc50dadd0b1e9f80b9b10e2dc6e3
weixiaoqiang <weixiaoqiang@cvte.com>
2021-10-30 22:47:36 +0800

    update .gitignore

---
+++ webview_flutter/example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/weixiaoqiang/Desktop/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/weixiaoqiang/Desktop/project/webview_flutter_enable_transparent/webview_flutter/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/weixiaoqiang/Desktop/project/webview_flutter_enable_transparent/webview_flutter/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/weixiaoqiang/Desktop/project/webview_flutter_enable_transparent/webview_flutter/example/.dart_tool/package_config.json"
